f7a8cdb97d5956d7f989c74e00aeb37a4692f886
refilwe06 <refilwemotseo2@gmail.com>
2024-09-16 16:27:20 +0200

    fix deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "https://figma-backend-mm5f.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   })
