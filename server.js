16b7bfd61238abce435b2f7e1f174b980c6f27aa
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-14 21:24:56 +0200

    fix: attempt to resolve render CORS issues in PROD

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cookieParser());
 
 app.use(
